7e834107ce12fc76a41b6d13c1806400ceab18eb
nguyensythien <nguyensythien19979@gmail.com>
2023-09-28 13:44:50 +0700

    first commit

---
+++ js/controllers/main.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function onForm() {
+    let addClass = document.querySelector('#exampleModal');
+    addClass.classList.add('show')
+}
+function offForm() {
+    let removeClass = document.querySelector('#exampleModal');
+    removeClass.classList.remove('show')
+}
